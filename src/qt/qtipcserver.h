0aafc716c2f8524856ebd8d29df1033d91dddd0f
Instantbitex <instantbitex@gmail.com>
2018-05-28 18:21:27 +0530

    Source Code

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Instantbitex-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "InstantbitexURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
